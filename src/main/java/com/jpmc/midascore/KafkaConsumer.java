e574760d2dde46d9e48bf8c9fbdf3de1f81aa9f6
Harshit Jain <jainharshit2132005@gmail.com>
2025-11-20 22:21:22 +0530

    Task 3 done

--- src/main/java/com/jpmc/midascore/KafkaConsumer.java
+++ src/main/java/com/jpmc/midascore/KafkaConsumer.java
@@ -1,36 +1,28 @@
 package com.jpmc.midascore;
 
 import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.service.TransactionService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class KafkaConsumer {
 
-    private List<Transaction> receivedTransactions = new ArrayList<>();
+    private final TransactionService transactionService;
+
+    public KafkaConsumer(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
 
     @KafkaListener(
             topics = "${general.kafka-topic}",
-            groupId = "midas-group-fresh",  // ← CHANGED THIS
+            groupId = "midas-group-task3",
             properties = {
-                    "auto.offset.reset=earliest"  // ← ADDED THIS
+                    "auto.offset.reset=earliest"
             }
     )
     public void listen(Transaction transaction) {
-        receivedTransactions.add(transaction);
         System.out.println("Received transaction: " + transaction);
-
-        // Set breakpoint HERE on line 18
-        if (receivedTransactions.size() <= 4) {
-            System.out.println("Transaction #" + receivedTransactions.size() +
-                    " Amount: " + transaction.getAmount());
-        }
-    }
-
-    public List<Transaction> getReceivedTransactions() {
-        return receivedTransactions;
+        transactionService.processTransaction(transaction);
     }
 }
